dd42383af7110902584cccabca296fada2dbe4ce
Ben Saufley <subscriptions@bensaufley.com>
2013-08-14 20:39:30 -0400

    Starting to build basic layout

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,4 +1,6 @@
 class ClubsController < ApplicationController
+  
+  before_filter :get_club, except: :index
 
   def index
     @clubs = Club.all.order('city ASC')
@@ -7,8 +9,17 @@ class ClubsController < ApplicationController
       format.json { render json: @clubs }
     end
   end
+  
+  def show
+    @classes = @club.gx_class_sets.where(params)
+  end
 
   private
+  
+  def get_club
+    @club = Club.find_by(cAbbrv: params[:club_id])
+  end
+
   def club_params
     params.require(:club).permit(:id,:cAbbrv,:city,:address,:brand,:phone,:link,:sfdc_name,:fb,:twitter,:timezone)
   end
--- app/models/club.rb
+++ app/models/club.rb
@@ -4,6 +4,8 @@ class Club < ActiveRecord::Base
   has_many :club_instructors
   has_many :instructors, through: :club_instructors
   has_many :studios
+  has_many :gx_classes
+  has_many :gx_class_sets, through: :gx_classes
   
   def to_param
     self.cAbbrv
